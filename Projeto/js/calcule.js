f43dda1133b5efb4dfc611cc3b87ba4f99cda5aa
Lincoln Ferreira Campos <Link7-7@hotmail.com>
2016-09-25 22:27:14 -0300

    Sistema que inclui elementos HTML e seus dados, também calcula o IMC

--- Projeto/js/calcule.js
+++ Projeto/js/calcule.js
@@ -1,24 +1,18 @@
-
-function calculaTodosIMC(){
-	var pacientes = document.getElementsByClassName("paciente")
-
-	percorreArray(pacientes, function(trPaciente){		
-
-			var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
-			var pacienteAtual = pessoa(trPaciente);
-
-			var imc = pacienteAtual.calculaIMC();		
-			tdImc.textContent = imc;
-		}
-	);
-};
-
 var botao = document.getElementById("calcula-imcs");
-/*
-botao.onclick = calculaTodosIMC;*/
+/*botao.onclick = calculaTodosIMC;*/
 
-botao.addEventListener("click", calculaTodosIMC);
 botao.addEventListener("click", function(){
-	console.log("Texto qual quer!")
-})
+		var pacientes = document.getElementsByClassName("paciente")
 
+		percorreArray(pacientes, function(trPaciente){		
+
+				var tdImc = trPaciente.getElementsByClassName("info-imc")[0];
+				var pacienteAtual = pessoa(trPaciente);
+
+				var imc = pacienteAtual.calculaIMC();		
+				tdImc.textContent = imc;
+			}
+		);
+	}
+
+);
